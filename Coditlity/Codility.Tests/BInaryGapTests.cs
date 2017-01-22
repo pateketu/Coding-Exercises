430636f6a00cd2864dea274ec75cb8e535bfa652
Ketul Patel <ketul.patel@GrosvenorSystems.com>
2017-01-22 15:32:22 +0000

    adding xUnit

---
+++ Coditlity/Codility.Tests/BInaryGapTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Codility.Tests
+{
+    public class BinaryGapTests
+    {
+        [Fact]
+        public void GetBinaryGap()
+        {
+            BinaryGap binrBinaryGap = new BinaryGap();
+           int gap = binrBinaryGap.GetBinaryGap(0);
+            Assert.Equal(0,gap);
+
+        }
+    }
+}
---
+++ Coditlity/Codility/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Codility")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c4e4919d-49ca-42c4-b2d2-ed4719a4ca68")]
